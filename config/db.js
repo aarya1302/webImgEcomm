80e4c8bd62f4d4fcb458ec609ed9c2a92e9876aa
Aarya Bhorra <1302ab11@gmail.com>
2021-08-18 21:15:39 +0400

    changes

---
+++ config/db.js
@@ -0,0 +1,30 @@
+const MongoClient = require("mongodb").MongoClient;
+const MongoOptions = require("mongodb").MongoOptions;
+require("dotenv").config({ path: ".env" });
+
+let uri = process.env.MONGO_URI;
+
+let dbname = "ecomm_db";
+
+var state = {
+  db: null,
+};
+
+var connect = (cb) => {
+  if (state.db) cb();
+  else {
+    MongoClient.connect(uri, MongoOptions, (err, client) => {
+      if (err) cb(err);
+      else {
+        state.db = client.db(dbname);
+        cb();
+      }
+    });
+  }
+};
+
+var getDB = () => {
+  return state.db;
+};
+
+module.exports = { getDB, connect };
